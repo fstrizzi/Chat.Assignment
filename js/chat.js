52b8445b4cc2039efb60ad7e2ecdd5a42244e054
fstrizzi <facundostrizzi@gmail.com>
2017-06-27 19:08:24 +0200

    comments & descriptions
    Add comments and descriptions

--- js/chat.js
+++ js/chat.js
@@ -6,12 +6,15 @@ jQuery(document).ready(function() {
 }),
 BCHAT = function (e, n) {
 	return e.chat = {
+		/**
+		  * @desc Initiate chat
+		*/
 		init: function() {
 			var e = this;
-			var submitting = false;
+			var submitting = false; //to prevent double submits
 			BCHAT.noActivity = 0;
 			BCHAT.lastID = null;
-			//submit login
+			//submit login form
 			n('#loginForm').submit(function(t){
 				t.preventDefault()
 				if(submitting) return false;
@@ -26,25 +29,30 @@ BCHAT = function (e, n) {
 				var name = n('input[name="name"]').val();
 				var email = n('input[name="email"]').val();
 
+				//If name form is empty return error
 				if(name.length == 0){
 					BCHAT.functions.displayError('Error')
 					submitting = false;
 					return false;
 				}
+				//Do login
 				e.login(name,email);
 				return false;
 			});
 
-			//submit chat text
+			//submit chat text form
 			n('#submitForm').submit(function(t){
 				t.preventDefault()
 				var text = n('#chatText').val();
+
+				//if field is empty
 				if(text.length == 0){
 					return false;
 				}
-				//if(submitting) return false;
+				if(submitting) return false;
 				submitting = true;
 
+				//check conversation ID
 				var convId = n('ul.chat:visible').attr('id');
 				
 				// Assigning a temporary ID to the chat:
@@ -58,7 +66,7 @@ BCHAT = function (e, n) {
 
 				// Add text to screen				
 				BCHAT.functions.addChatText(n.extend({},params));
-				n('#chatText').val('');
+				n('#chatText').val(''); //empty field
 
 				/* // send POST AJAX request:
 				$.bPost(/conversation/:conversationid/message/send,n(this).serialize(),function(r){
@@ -70,22 +78,24 @@ BCHAT = function (e, n) {
 					params['id'] = r.insertID;
 					BCHAT.functions.addChatText(n.extend({},params));
 				});*/
+				submitting = false;
 				return false;
 			});
 		}, 
-		isUserLoggedIn: function () {
-			return;
-		}, 
+		/**
+		  * @desc provides the login in the chat
+		  * @param string $name - user name
+		  * @param string $email - user email
+		*/
 		login: function(name, email){
-			BCHAT.name = name;
+			BCHAT.name = name; //set user name
 			n('#loginForm').fadeOut(function(){
-				
-				n('#collapseChat .panel-body').prepend(BCHAT.templates.render('userList',null));
+				n('#collapseChat .panel-body').prepend(BCHAT.templates.render('userList',null)); //render userList
 				(function getUsersTimeoutFunction(){
-					e.functions.getUsers(getUsersTimeoutFunction);
+					e.functions.getUsers(getUsersTimeoutFunction); //get Users
 				})();
-				e.functions.contactLinks();
-				n('#collapseChat .panel-body').removeClass('panel-body-sm');
+				e.functions.contactLinks(); //add click to newly created users
+				n('#collapseChat .panel-body').removeClass('panel-body-sm'); //Remove class on body
 			});
 		}
 	}, e
@@ -93,8 +103,11 @@ BCHAT = function (e, n) {
 BCHAT = function(e, n) {
 	"use strict";
 	return e.menu = {
+		/**
+		  * @desc menu and visual functions
+		*/
         init: function() {
-            n(".btn-chatToggle").click(function() {
+            n(".btn-chatToggle").click(function() { //Close or open chat window
             	var t = n(this).children('.glyphicon');
 				n("#collapseChat").hasClass("collapse in") ? (
 				    e.menu.closeChat(), t.removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up')
@@ -103,25 +116,39 @@ BCHAT = function(e, n) {
 				);
             })
         },
+        /**
+		  * @desc open chat window
+		*/
         openChat: function() {
             n("#collapseChat").collapse('show')
         },
+        /**
+		  * @desc close chat window
+		*/
         closeChat: function() {
             n("#collapseChat").collapse('hide')
         },
+        /**
+		  * @desc open contact list
+		*/
         openContacts: function() {
 			n('#userlist').removeClass('hidden');
 	        if ( n('.btn-contacts-toggle').length ) {
 	            n('.btn-contacts-toggle').remove()
 	        }
+	        //hide all chats
             n("ul.chat").each( function () {
             	n(this).addClass('hidden');
             });
+            //set chat title
             n('.chat-title').html(' Chat')
         },
+        /**
+		  * @desc close contact list
+		*/
         closeContacts: function() {
-            n('#userlist').addClass('hidden');
-	        if ( !n('.btn-contacts-toggle').length ) {
+            n('#userlist').addClass('hidden'); //hide userlist
+	        if ( !n('.btn-contacts-toggle').length ) { //if button don't exists create it
 	        	var contactsToggle = n('<a/>', {
 			        html: '<span class="glyphicon glyphicon-menu-left"></span>',
 			        type: 'button',
@@ -132,13 +159,19 @@ BCHAT = function(e, n) {
 			        }
 			    });
 
-			   n('#chatButtons').prepend(contactsToggle);
+			   n('#chatButtons').prepend(contactsToggle); //add button
 	        }
         },
+        /**
+		  * @desc show message field
+		*/
         showSubmitForm: function() {
         	n('#submitForm').removeClass('hidden');	
         	n('#chatText').focus();
         },
+        /**
+		  * @desc hide message field
+		*/
         hideSubmitForm: function() {
         	n('#submitForm').addClass('hidden');	
         }
@@ -147,15 +180,34 @@ BCHAT = function(e, n) {
 BCHAT = function (e, n) {
 	"use strict";
 	return e.functions = {
+		/**
+		  * @desc General Functions: POST, GET, Obtain users, add chat text, etc
+		*/
 		init: function () {
 			BCHAT.users = [];
 		},
+		/**
+		  * @desc general GET function
+		  * @param string $action - url to get
+		  * @param string $data - data to send
+		  * @param function $callback - A callback function that is executed if the request succeeds
+		*/
 		bGet: function (action,data,callback) {
 			n.get(action,data,callback,'json');
 		},
+		/**
+		  * @desc general POST function
+		  * @param string $action - url to post to
+		  * @param string $data - data to send
+		  * @param function $callback - A callback function that is executed if the request succeeds
+		*/
 		bPost: function (action,data,callback) {
-			n.get(+action,data,callback,'json');
+			n.post(+action,data,callback,'json');
 		},
+		/**
+		  * @desc get users list
+		  * @param function $callback - A callback function that is executed if the request succeeds
+		*/
 		getUsers: function(callback){
 			this.bGet('http://assignment.bunq.com/users',function(r){
 				var users = [], len;
@@ -175,10 +227,14 @@ BCHAT = function (e, n) {
 				);
 				
 				users.push('<p class="count">'+message+'</p>');				
-				n('#collapseChat #userlist').html(users.join(''));
-				setTimeout(callback,15000);
+				n('#collapseChat #userlist').html(users.join('')); //add to userlist
+				setTimeout(callback,15000); //check every 15sec if new users
 			});
 		},
+		/**
+		  * @desc general GET function
+		  * @param obj $params - message parameters: id, text, author, time
+		*/
 		addChatText: function(params){
 			// All times are displayed in user's timezone
 			var d = new Date();
@@ -189,14 +245,15 @@ BCHAT = function (e, n) {
 			params.time = (d.getHours() < 10 ? '0' : '' ) + d.getHours()+':'+
 						  (d.getMinutes() < 10 ? '0':'') + d.getMinutes();
 			
+			//Render if message is from me
 			if (params.author == BCHAT.name) {
 				var markup = BCHAT.templates.render('chatLineR',params);
 			} 
 			else var markup = BCHAT.templates.render('chatLineL',params);
 			
-			
+			//check if message exists
 			var exists = n('#chatLineHolder .chat-'+params.id);
-
+			//if exists remove it
 			if(exists.length){
 				exists.remove();
 			}
@@ -211,6 +268,10 @@ BCHAT = function (e, n) {
 			}
 			else n('#'+params.convId).append(markup);
 		},
+		/**
+		  * @desc get Old Chats
+		  * @param function $callback - A callback function that is executed if the request succeeds
+		*/
 		getChats: function(callback){
 			/* //getConversations
 			$.bGet(/conversation/:id,{lastID: BCHAT.lastID},function(r){
@@ -249,6 +310,10 @@ BCHAT = function (e, n) {
 				setTimeout(callback,nextReq);
 			});*/
 		},
+		/**
+		  * @desc provides functionality to each contact
+		  * @param function $callback - A callback function that is executed if the request succeeds
+		*/
 		contactLinks : function (callback) {
 			n('#userlist').on('click', 'li', function(t){
 				var userId = n(this).attr('id').replace('user-', '');
@@ -260,6 +325,10 @@ BCHAT = function (e, n) {
 				e.functions.openConversation(params);
             })
 		},
+		/**
+		  * @desc open or create new conversation
+		  * @param obj $params - userId, name
+		*/
 		openConversation: function (params) {
 			e.menu.showSubmitForm();
 			e.menu.closeContacts();
@@ -283,6 +352,10 @@ BCHAT = function (e, n) {
 				e.functions.getChats(getOldChatsFunction);
 			})();*/
 		},
+		/**
+		  * @desc show errors
+		  * @param string $msg message text
+		*/
 		displayError : function(msg){
 			var elem = n('<div>',{
 				id		: 'chatErrorMessage',
@@ -290,53 +363,57 @@ BCHAT = function (e, n) {
 				html	: msg
 			});
 			
-			elem.click(function(){
+			elem.click(function(){ //if click hide it
 				n(this).fadeOut(function(){
 					n(this).remove();
 				});
 			});
 			
-			setTimeout(function(){
+			setTimeout(function(){ //hide it after 5 sec
 				elem.click();
 			},5000);
 			
-			elem.hide().appendTo('body').slideDown();
+			elem.hide().appendTo('body').slideDown(); //add it to window
 		}
 	}, e
 }(BCHAT || {}, jQuery),	
 BCHAT = function(e, n) {
 	"use strict";
 	return e.templates = {
+		/**
+		  * @desc Render elements
+		*/
 		init: function () {
 			
-		}, 
+		},
+		/**
+		  * @desc General Functions: POST, GET, Obtain users, add chat text, etc
+		  * @param string $template template to render
+		  * @param obj $params template parameters
+		*/
 		render : function(template,params){
 			var arr = [];
 			switch(template){
-				case 'chatLineR':
+				case 'chatLineR': //chat message right
 					arr = [
 					'<li class="right clearfix chat chat-',params.id,'"><span class="chat-img pull-right"><img src="http://placehold.it/50/55C1E7/fff&text=U" alt="',params.author,' Avatar" class="img-circle" /></span><div class="chat-body clearfix"><div class="header"><small class=" text-muted"><span class="glyphicon glyphicon-time"></span>',params.time,'</small><strong class="pull-right primary-font">',params.author,
 						'</strong></div><p>',params.text,'</p></div></li>'];
 				break;
-				case 'chatLineL':
+				case 'chatLineL': //chat message left
 					arr = [
 					'<li class="left clearfix chat chat-',params.id,'"><span class="chat-img pull-left"><img src="http://placehold.it/50/55C1E7/fff&text=U" alt="',params.author, ' Avatar" class="img-circle" /></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">',params.author,'</strong> <small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span>',params.time,'</small></div><p>',params.text,'</p></div></li>']
 				break;
-				case 'userList':
+				case 'userList':  //userlist
 					arr = ['<ul id="userlist"></ul>'];
 					break;
-				case 'chatList':
+				case 'chatList':  //chat window
 					arr = ['<ul class="chat" id="',params.id,'"></ul>'];
 					break;
-				case 'user':
+				case 'user':  //users
 					arr = ['<li class="left clearfix" id="user-',params.id,'"><span class="user-img pull-left"><img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" /></span><div class="user-body clearfix"><div class="header"><strong class="primary-font">',params.name,'</strong></div></div></li>'];
-				break;
-				case 'contactsButton':
-					arr = ['<a type="button" class="btn btn-link btn-xs btn-contacts-toggle"><span class="glyphicon glyphicon-user"></span></a>'];
 				break;
 			}
 			return arr.join('');
-			
 		}
 	},e
 }(BCHAT || {}, jQuery)
